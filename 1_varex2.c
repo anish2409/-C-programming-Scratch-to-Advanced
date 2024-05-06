e4a81c9423a6763efa4616781c946197cd9f4434
anish2409 <139654946+anish2409@users.noreply.github.com>
2024-05-06 23:50:18 +0530

    Add files via upload

---
+++ 1_varex2.c
@@ -0,0 +1,14 @@
+//Area of Circle
+
+#include<stdio.h>
+
+int main(){
+
+    float radious;
+    printf("Enter radious:");
+    scanf("%f",&radious);
+
+    printf("Area is : %f",3.14 * radious*radious);
+    return 0 ;
+
+}
