87911a3588d6bdd3b19964fc8adee5774f234a60
anish2409 <139654946+anish2409@users.noreply.github.com>
2024-05-06 23:48:29 +0530

    Add files via upload

---
+++ 1_variables.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(){
+    int number= 25;
+    char str= '*';
+    int age ;
+    age = 24;
+    float pi =2.4;
+    
+
+    int z = 30;
+    int Z= 40;
+
+    int _age= 22;
+    char hashtag= '#';
+
+    printf("Age is %d\n",_age);
+    printf("float is %f\n", pi);
+    printf("Star is %c\n", str);
+    /*
+    printf("Enter age");
+    scanf("%d", &age);
+    printf("Age is %d\n",age);
+    return 0;
+    */
+    int a,b;
+    printf("enter a");
+    scanf("%d",&a);
+    printf("enter b");
+    scanf("%d",&b);
+
+    int sum = a+b;
+    printf("Sum of %d and %d is %d \n" ,a,b,sum );
+}
+/*
+    Multiple line comments
+*/
