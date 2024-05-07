8870a416fc349f0340baf9c293e6d2e3217184ba
anish2409 <139654946+anish2409@users.noreply.github.com>
2024-05-07 12:02:54 +0530

    Add files via upload

---
+++ 2_operator_ex1.c
@@ -0,0 +1,13 @@
+// Write a program to check if a number is divisible by 2 or not
+// even or odd
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the  number: ");
+    scanf("%d",&n);
+    printf("%d",n%2==0);
+    return 0 ;
+
+
+}
