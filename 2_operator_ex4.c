898c4fe0c9282cdd92d29283b26112bc03020721
anish2409 <139654946+anish2409@users.noreply.github.com>
2024-05-08 12:37:33 +0530

    Add files via upload

---
+++ 2_operator_ex4.c
@@ -0,0 +1,23 @@
+//Write a program to print the smallest number
+
+#include<stdio.h>
+int main(){
+    /*
+    int num1, num2;
+    printf("Enter first number :");
+    scanf("%d", &num1);
+    printf("\nEnter second number:");   
+    scanf("%d",&num2);
+    if(num1 < num2)
+        printf("\nSmallest Number is %d", num1);
+    else  
+        printf("\nSmallest Number is %d", num2);  
+    return 0;       
+    */
+   int  x=0,y=0;
+
+    if(x && y++)
+        printf("%d %d",x++,y);
+    else
+        printf("%d %d",x,y);
+}
