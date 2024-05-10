4b154f82ac35638251e97b87374582d0b7c974f6
anish2409 <139654946+anish2409@users.noreply.github.com>
2024-05-10 23:24:16 +0530

    Add files via upload

---
+++ 3_conditionalstatements_ex3.c
@@ -0,0 +1,19 @@
+/*Will this code: 
+a.give error
+b.print "x is equal 1"
+c.print "x is notequal 1"
+*/
+
+#include<stdio.h>
+
+int main(){
+    int x=2;
+
+    if(x=1){
+        printf("X is equal to 1\n");
+    }
+    else{
+        printf("X is not equal to 1");
+    }
+
+}
