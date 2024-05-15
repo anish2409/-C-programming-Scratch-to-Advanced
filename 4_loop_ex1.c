cf43746b96729da2feb8d7b249f65c6a880d008d
anish2409 <139654946+anish2409@users.noreply.github.com>
2024-05-15 10:19:01 +0530

    Add files via upload

---
+++ 4_loop_ex1.c
@@ -0,0 +1,14 @@
+//Print the table of a number input by the user.
+//n=2
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    for(int i=1;i<10;i++){
+        printf("%d \n",n*i);
+    }
+    return 0;
+
+}
