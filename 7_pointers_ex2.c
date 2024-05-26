e58f6854542db221604dac165e2407a05fe3c349
Anish Das <139654946+anish2409@users.noreply.github.com>
2024-05-26 10:56:12 +0530

    Add files via upload

---
+++ 7_pointers_ex2.c
@@ -0,0 +1,29 @@
+//Swap 2 numbers, a&b
+#include<stdio.h>
+
+void swap(int a , int b);
+void _swap(int *a,int *b);
+int main(){
+    int x=3;
+    int y=5;
+    //swap(x,y);
+    //printf("x=%d & y=%d\n",x,y);
+    _swap(&x,&y);
+    printf("x=%d & y=%d\n",x,y);
+
+    return 0;
+}
+//Call by Reference
+void _swap(int *a,int *b){
+    int t =*a;
+    *a=*b;
+    *b=t;
+}
+
+//Call by value
+void swap(int a , int b){
+    int t=a;
+    a=b;
+    b=t;
+    printf("a=%d & b=%d\n",a,b);
+}
